01abb49b3b909e3f489f33f4d19e648e0f1a2a3c
Fatma Aktaş <fatmaaaktass10@gmail.com>
2022-09-21 23:44:29 +0300

    Pdf_example

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pdf.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PDFdonustur(int id)
+        {
+            site.m_yeni_dosya = db.tbl_yeni_dosya.Where(t => t.yeni_dosya_id == id).FirstOrDefault();
+
+            return View("PDFdonustur", site);
+    }
+}
+}
